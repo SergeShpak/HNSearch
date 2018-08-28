75ef2f1514792adf734b50985bf8f9480d1e54ab
Sergey Shpak <sshpak92@gmail.com>
2018-08-28 11:00:19 +0200

    feat(first-try): add dateTime UTC representation

--- server/handlers/date.go
+++ server/handlers/date.go
@@ -4,71 +4,108 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 
 	"github.com/SergeyShpak/HNSearch/server/utils"
 )
 
+func getTimeUnitWithHeadingZeroes(timeUnit string) string {
+	if len(timeUnit) == 2 {
+		return timeUnit
+	}
+	return "0" + timeUnit
+}
+
 type date struct {
-	Year  *string
-	Month *string
-	Day   *string
+	year  *string
+	month *string
+	day   *string
+
+	repr string
 }
 
 func newDate(dateStr *string) (*date, error) {
 	if dateStr == nil {
-		return nil, nil
+		return nil, fmt.Errorf("nil dateString passed")
 	}
 	dParts := utils.ReExtractNumbers(*dateStr)
 	datePartsCount := 3
 	if len(dParts) == 0 || len(dParts) > datePartsCount {
 		return nil, fmt.Errorf("expected from 1 to %d date parts, actual number: %d", datePartsCount, len(dParts))
 	}
-	d := &date{}
-	if len(dParts) >= 1 {
-		d.Year = &dParts[0]
+	yearStr := dParts[0]
+	dStrParts := []string{yearStr, "01", "01"}
+	d := &date{
+		year: &yearStr,
 	}
 	if len(dParts) >= 2 {
-		d.Month = &dParts[1]
+		monthStr := getTimeUnitWithHeadingZeroes(dParts[1])
+		d.month = &monthStr
+		dStrParts[1] = monthStr
 	}
 	if len(dParts) >= 3 {
-		d.Day = &dParts[2]
+		dayStr := getTimeUnitWithHeadingZeroes(dParts[2])
+		d.day = &dayStr
+		dStrParts[2] = dayStr
 	}
+	d.repr = strings.Join(dStrParts, "-")
 	return d, nil
+}
+
+func (d *date) String() string {
+	return d.repr
 }
 
 type time struct {
 	Hour   *string
 	Minute *string
 	Second *string
+
+	repr string
 }
 
 func newTime(timeStr *string) (*time, error) {
+	t := &time{}
 	if timeStr == nil {
-		return nil, nil
+		t.repr = "00:00:00"
+		return t, nil
 	}
 	tParts := utils.ReExtractNumbers(*timeStr)
 	timePartsCount := 3
 	if len(tParts) == 0 || len(tParts) > timePartsCount {
 		return nil, fmt.Errorf("expected from 1 to %d date parts, actual number: %d", timePartsCount, len(tParts))
 	}
-	t := &time{}
-	if len(tParts) >= 1 {
-		t.Hour = &tParts[0]
-	}
+	hourStr := getTimeUnitWithHeadingZeroes(tParts[0])
+	tStrParts := []string{hourStr, "00", "00"}
+	t.Hour = &hourStr
 	if len(tParts) >= 2 {
+		minuteStr := getTimeUnitWithHeadingZeroes(tParts[1])
+		tStrParts[1] = minuteStr
 		t.Minute = &tParts[1]
 	}
 	if len(tParts) >= 3 {
+		secondStr := getTimeUnitWithHeadingZeroes(tParts[2])
+		tStrParts[2] = secondStr
 		t.Second = &tParts[2]
 	}
+	t.repr = strings.Join(tStrParts, ":")
 	return t, nil
+}
+
+func (t time) String() string {
+	return t.repr
 }
 
 type dateTime struct {
 	date *date
 	time *time
+}
+
+func (dt dateTime) String() string {
+	repr := dt.date.String() + "T" + dt.time.String() + "Z"
+	return repr
 }
 
 func newDateTime(dateTimeStr string) (*dateTime, error) {
@@ -104,7 +141,7 @@ func DateDistinctHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(err.Error()))
 		return
 	}
-	msg := fmt.Sprintf("Date: %v", dt.date)
+	msg := fmt.Sprintf("Date: %v", dt)
 	w.Write([]byte(msg))
 }
 
