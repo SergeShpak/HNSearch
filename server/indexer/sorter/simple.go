070128176115ee1658fe4833421c1bcb6261ced8
Sergey Shpak <sshpak92@gmail.com>
2018-09-04 08:48:25 +0200

    refactor(indexing): use configuration in the sorter's code (#3)

--- server/indexer/sorter/simple.go
+++ server/indexer/sorter/simple.go
@@ -1,5 +1,11 @@
 package sorter
 
+/*
+TODO:
+	1. Move random file generation to a subroutine (use github.com/rs/xid)
+	2. Add configuration checking
+*/
+
 import (
 	"bufio"
 	"fmt"
@@ -8,38 +14,58 @@ import (
 	"os"
 	"sort"
 	"time"
+
+	"github.com/SergeyShpak/HNSearch/server/indexer/config"
+	"github.com/SergeyShpak/HNSearch/server/indexer/parser"
 )
 
-type SimpleSorter struct{}
-
-func NewSimpleSorter() *SimpleSorter {
-	sorter := &SimpleSorter{}
-	return sorter
+type simpleSorter struct {
+	config *config.SimpleSorter
+	parser parser.Parser
 }
 
-func (sorter *SimpleSorter) SortSet(r io.Reader) error {
+func newSimpleSorter(c *config.SimpleSorter) (*simpleSorter, error) {
+	sorter := &simpleSorter{
+		config: c,
+	}
+	var err error
+	sorter.parser, err = parser.NewParser(c.Parser)
+	if err != nil {
+		return nil, fmt.Errorf("error while initializing sorter's parser: %v", err)
+	}
+	return sorter, nil
+}
+
+func (sorter *simpleSorter) SortSet(r io.Reader) (string, error) {
 	tmpDir, err := sorter.splitLargeSet(r)
 	if err != nil {
-		return err
+		return "", err
 	}
 	resultFile, err := sorter.mergeDataSets(tmpDir)
 	if err != nil {
-		os.RemoveAll(tmpDir)
-		return err
+		//os.RemoveAll(tmpDir)
+		return "", err
 	}
-	if err := os.Rename(resultFile, "sorted.tsv"); err != nil {
-		os.RemoveAll(tmpDir)
-		return err
+	fmt.Println("Here!")
+	resultName, err := sorter.generateResultName(resultFile)
+	if err != nil {
+		//os.RemoveAll(tmpDir)
+		return "", err
 	}
-	os.RemoveAll(tmpDir)
-	return nil
+	sortedFile, err := moveFile(resultFile, sorter.config.OutDir, resultName)
+	if err != nil {
+		//os.RemoveAll(tmpDir)
+		return "", err
+	}
+	//os.RemoveAll(tmpDir)
+	return sortedFile, nil
 }
 
-func (sorter *SimpleSorter) splitLargeSet(r io.Reader) (string, error) {
+func (sorter *simpleSorter) splitLargeSet(r io.Reader) (string, error) {
 	scanner := bufio.NewScanner(r)
-	maxBufLen := 50 * 1024 * 1024 // 50 MB
+	maxBufLen := sorter.config.Buffer
 	lines := make([]string, 0)
-	currLen := 0
+	var currLen uint64
 	chunkNumber := 0
 	dir := fmt.Sprintf("tmp_chunks_%v", time.Now().Unix())
 	if err := os.MkdirAll(dir, 0777); err != nil {
@@ -65,7 +91,7 @@ func (sorter *SimpleSorter) splitLargeSet(r io.Reader) (string, error) {
 	}
 	for scanner.Scan() {
 		line := scanner.Text() + "\n"
-		currLen += len(line)
+		currLen += uint64(len(line))
 		lines = append(lines, line)
 		if currLen > maxBufLen {
 			if err := writeToFileFn(); err != nil {
@@ -86,7 +112,7 @@ func (sorter *SimpleSorter) splitLargeSet(r io.Reader) (string, error) {
 	return dir, nil
 }
 
-func (sorter *SimpleSorter) mergeDataSets(dir string) (string, error) {
+func (sorter *simpleSorter) mergeDataSets(dir string) (string, error) {
 	files, err := getFiles(dir)
 	if err != nil {
 		return "", err
@@ -95,8 +121,8 @@ func (sorter *SimpleSorter) mergeDataSets(dir string) (string, error) {
 		return "", fmt.Errorf("No files found in the directory \"%s\"", dir)
 	}
 	if len(files) == 1 {
-		// TODO: change
-		return "", nil
+		path := fmt.Sprintf("%s%c%s", dir, os.PathSeparator, files[0].Name())
+		return path, nil
 	}
 	fileNames := make([]string, 0, len(files))
 	for _, f := range files {
@@ -109,8 +135,8 @@ func (sorter *SimpleSorter) mergeDataSets(dir string) (string, error) {
 	return resultFile, nil
 }
 
-func (sorter *SimpleSorter) mergeDataSetsAux(first string, second string, tail []string, tmpDir string) (string, error) {
-	tmpFile, err := mergeFiles(first, second, tmpDir)
+func (sorter *simpleSorter) mergeDataSetsAux(first string, second string, tail []string, tmpDir string) (string, error) {
+	tmpFile, err := sorter.mergeFiles(first, second, tmpDir)
 	if err != nil {
 		return "", err
 	}
@@ -118,13 +144,146 @@ func (sorter *SimpleSorter) mergeDataSetsAux(first string, second string, tail [
 		return tmpFile, nil
 	}
 	if len(tail) == 1 {
-		tmpFile, err = mergeFiles(tmpFile, tail[0], tmpDir)
+		tmpFile, err = sorter.mergeFiles(tmpFile, tail[0], tmpDir)
 		if err != nil {
 			return "", err
 		}
 		return tmpFile, err
 	}
 	return sorter.mergeDataSetsAux(tmpFile, tail[0], tail[1:], tmpDir)
+}
+
+func (sorter *simpleSorter) mergeFiles(first string, second string, tmpDir string) (string, error) {
+	fileMaxBuf := sorter.config.Buffer / 4
+	firstF, err := os.Open(first)
+	if err != nil {
+		return "", err
+	}
+	secondF, err := os.Open(second)
+	if err != nil {
+		return "", err
+	}
+	fScanner := bufio.NewScanner(firstF)
+	var fLines []string
+	var isFirstFinished bool
+	sScanner := bufio.NewScanner(secondF)
+	var sLines []string
+	var isSecondFinished bool
+	tmpFile, err := sorter.createTmpFile(tmpDir)
+	if err != nil {
+		return "", err
+	}
+	for {
+		if len(fLines) == 0 {
+			if isFirstFinished {
+				break
+			}
+			fLines, isFirstFinished = readFromScanner(fScanner, fileMaxBuf)
+		}
+		if len(sLines) == 0 {
+			if isSecondFinished {
+				break
+			}
+			sLines, isSecondFinished = readFromScanner(sScanner, fileMaxBuf)
+		}
+		var merged []string
+		merged, fLines, sLines = mergeLines(fLines, sLines)
+		if err := appendToFile(merged, tmpFile); err != nil {
+			return "", err
+		}
+	}
+	if len(fLines) != 0 {
+		for {
+			if err := appendToFile(fLines, tmpFile); err != nil {
+				return "", err
+			}
+			if isFirstFinished {
+				break
+			}
+			fLines, isFirstFinished = readFromScanner(fScanner, fileMaxBuf)
+		}
+		return tmpFile, nil
+	}
+	for {
+		if err := appendToFile(sLines, tmpFile); err != nil {
+			return "", err
+		}
+		if isSecondFinished {
+			break
+		}
+		sLines, isSecondFinished = readFromScanner(sScanner, fileMaxBuf)
+	}
+	return tmpFile, nil
+}
+
+func (sorter *simpleSorter) createTmpFile(tmpDir string) (string, error) {
+	timeNow := time.Now().Unix()
+	fileNameTempl := fmt.Sprintf("%s%cmerged_%%d", tmpDir, os.PathSeparator)
+	errCtr := 0
+	for {
+		if errCtr >= sorter.config.TmpCreationRetries {
+			break
+		}
+		fileName := fmt.Sprintf(fileNameTempl, timeNow)
+		if _, err := os.Stat(fileName); os.IsNotExist(err) {
+			f, err := os.Create(fileName)
+			if err != nil {
+				return "", err
+			}
+			if err := f.Close(); err != nil {
+				return "", err
+			}
+			return fileName, nil
+		}
+		timeNow++
+		errCtr++
+	}
+	return "", fmt.Errorf("could not create a new temporary file")
+}
+
+func (sorter *simpleSorter) generateResultName(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	firstLine := scanner.Text()
+	q, err := sorter.parser.Parse(firstLine)
+	if err != nil {
+		fmt.Println("First line: ", firstLine)
+		return "", err
+	}
+	firstDate := q.Date.Unix()
+	var line string
+	for scanner.Scan() {
+		line = scanner.Text()
+	}
+	q, err = sorter.parser.Parse(line)
+	if err != nil {
+		fmt.Println("Last line: ", line)
+		return "", err
+	}
+	secondDate := q.Date.Unix()
+	resultName := fmt.Sprintf("sorted_%d_%d", firstDate, secondDate)
+	return resultName, nil
+}
+
+func readFromScanner(scanner *bufio.Scanner, maxBuf uint64) ([]string, bool) {
+	lines := make([]string, 0)
+	var linesLen uint64
+	for scanner.Scan() {
+		l := scanner.Text()
+
+		lines = append(lines, l)
+		linesLen += uint64(len(l))
+		if linesLen >= maxBuf {
+			return lines, false
+		}
+	}
+	return lines, true
 }
 
 func dumpToFile(fileName string, data []string) error {
@@ -156,84 +315,6 @@ func getFiles(dirName string) ([]os.FileInfo, error) {
 	return dirEntries, nil
 }
 
-func mergeFiles(first string, second string, tmpDir string) (string, error) {
-	maxBuf := 12 * 1024 * 1024
-	firstF, err := os.Open(first)
-	if err != nil {
-		return "", err
-	}
-	secondF, err := os.Open(second)
-	if err != nil {
-		return "", err
-	}
-	fScanner := bufio.NewScanner(firstF)
-	var fLines []string
-	var isFirstFinished bool
-	sScanner := bufio.NewScanner(secondF)
-	var sLines []string
-	var isSecondFinished bool
-	tmpFile, err := createTmpFile(tmpDir)
-	if err != nil {
-		return "", err
-	}
-	for {
-		if len(fLines) == 0 {
-			if isFirstFinished {
-				break
-			}
-			fLines, isFirstFinished = readFromScanner(fScanner, maxBuf)
-		}
-		if len(sLines) == 0 {
-			if isSecondFinished {
-				break
-			}
-			sLines, isSecondFinished = readFromScanner(sScanner, maxBuf)
-		}
-		var merged []string
-		merged, fLines, sLines = mergeLines(fLines, sLines)
-		if err := appendToFile(merged, tmpFile); err != nil {
-			return "", err
-		}
-	}
-	if len(fLines) != 0 {
-		for {
-			if err := appendToFile(fLines, tmpFile); err != nil {
-				return "", err
-			}
-			if isFirstFinished {
-				break
-			}
-			fLines, isFirstFinished = readFromScanner(fScanner, maxBuf)
-		}
-		return tmpFile, nil
-	}
-	for {
-		if err := appendToFile(sLines, tmpFile); err != nil {
-			return "", err
-		}
-		if isSecondFinished {
-			break
-		}
-		sLines, isSecondFinished = readFromScanner(sScanner, maxBuf)
-	}
-	return tmpFile, nil
-}
-
-func readFromScanner(scanner *bufio.Scanner, max int) ([]string, bool) {
-	lines := make([]string, 0)
-	linesLen := 0
-	for scanner.Scan() {
-		l := scanner.Text()
-
-		lines = append(lines, l)
-		linesLen += len(l)
-		if linesLen >= max {
-			return lines, false
-		}
-	}
-	return lines, true
-}
-
 func mergeLines(first []string, second []string) ([]string, []string, []string) {
 	var fIdx int
 	var sIdx int
@@ -255,32 +336,6 @@ func mergeLines(first []string, second []string) ([]string, []string, []string) 
 	}
 }
 
-func createTmpFile(tmpDir string) (string, error) {
-	timeNow := time.Now().Unix()
-	fileNameTempl := fmt.Sprintf("%s%cmerged_%%d", tmpDir, os.PathSeparator)
-	errCtr := 0
-	for {
-		// TODO: make configurable??
-		if errCtr == 10 {
-			break
-		}
-		fileName := fmt.Sprintf(fileNameTempl, timeNow)
-		if _, err := os.Stat(fileName); os.IsNotExist(err) {
-			f, err := os.Create(fileName)
-			if err != nil {
-				return "", err
-			}
-			if err := f.Close(); err != nil {
-				return "", err
-			}
-			return fileName, nil
-		}
-		timeNow++
-		errCtr++
-	}
-	return "", fmt.Errorf("could not create a new temporary file")
-}
-
 func appendToFile(buf []string, file string) error {
 	f, err := os.OpenFile(file, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
@@ -298,4 +353,15 @@ func appendToFile(buf []string, file string) error {
 		return err
 	}
 	return nil
+}
+
+func moveFile(filePath string, newDir string, newFileName string) (string, error) {
+	if err := os.MkdirAll(newDir, 0777); err != nil {
+		return "", err
+	}
+	newFilePath := fmt.Sprintf("%s%c%s", newDir, os.PathSeparator, newFileName)
+	if err := os.Rename(filePath, newFilePath); err != nil {
+		return "", err
+	}
+	return newFilePath, nil
 }
--- server/indexer/sorter/sorter.go
+++ server/indexer/sorter/sorter.go
@@ -1,9 +1,23 @@
 package sorter
 
 import (
+	"fmt"
 	"io"
+
+	"github.com/SergeyShpak/HNSearch/server/indexer/config"
 )
 
 type Sorter interface {
-	SortSet(r io.Reader) error
+	SortSet(r io.Reader) (string, error)
+}
+
+func NewSorter(c *config.Config) (Sorter, error) {
+	if c.Sorter.Simple != nil {
+		sorter, err := newSimpleSorter(c.Sorter.Simple)
+		if err != nil {
+			return nil, err
+		}
+		return sorter, nil
+	}
+	return nil, fmt.Errorf("no sorter found in configuration")
 }
